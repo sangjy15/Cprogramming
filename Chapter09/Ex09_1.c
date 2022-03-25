8fc301190b89ae9c66432a76f2df0eebc9d05aa7
sangjy15 <40661788+sangjy15@users.noreply.github.com>
2022-03-25 23:25:25 +0900

    ex09_1

--- Chapter09/Ex09_1.c
+++ Chapter09/Ex09_1.c
@@ -22,9 +22,20 @@ double FahToCel(double fah) {
 }
 
 //ex03
-int pivonachi(int num) {
-	int i, result;
-	for (i = 0; i < num; i++) {
+void ShowFivoSeries(int num) {
+	int f1 = 0, f2 = 1, f3, i;
+	if (num == 1) {
+		printf("%d ", f1);
+	}
+	else {
+		printf("%d %d ", f1, f2);
+	}
+
+	for (i = 0; i < num - 2; i++) {
+		f3 = f1 + f2;
+		printf("%d ", f3);
+		f1 = f2;
+		f2 = f3;
 	}
 }
 
@@ -51,6 +62,13 @@ int main(void) {
 		printf("반환 된 섭씨 온도 : %lf", FahToCel(temp));
 	}
 	*/
-	
+	int n;
+	printf("출력하고자 하는 피보나치 수열의 갯수 : ");
+	scanf("%d", &n);
+	if (n < 1) {
+		printf("1이상의 값을 입력하세요. \n");
+		return -1;
+	}
+	ShowFivoSeries(n);
 	return 0;
 }
