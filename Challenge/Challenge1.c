24a05c29eab1ec76ae0d19eaf84cc8becc1c85c0
sangjy15 <40661788+sangjy15@users.noreply.github.com>
2022-03-28 22:06:22 +0900

    challenge1,2

---
+++ Challenge/Challenge1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable : 6031)
+#pragma warning(disable : 4996)
+
+void Chanllenge01() {
+	int num;
+	printf("정수를 입력하세요. \n");
+	scanf("%d ", &num);
+	printf("16진수로 변환한 값 : % \n");
+}
+
+void CompareNum(int num1, int num2) {
+	int i, j;
+	for (i = num1; i <= num2; i++) {
+		for (j = 1; j < 10; j++) {
+			printf("%d x %d = %d \n", i, j, i * i);
+		}
+		printf("============== \n");
+	}
+}
+
+void GetGooGooDan() {
+	int num1, num2;
+	printf("두 정수를 입력하세요. \n");
+	scanf("%d %d", &num1, &num2);
+	if (num1 > num2) 
+		CompareNum(num2, num1);
+	else 
+		CompareNum(num1, num2);
+}
+
+
+int main(void) {
+	// Chanllenge01();
+	GetGooGooDan();
+	return 0;
+}
