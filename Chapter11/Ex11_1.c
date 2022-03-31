91838236ef09e61196d2e76c20707cabbd9b2e7c
sangjy15 <40661788+sangjy15@users.noreply.github.com>
2022-03-31 22:21:59 +0900

    chapter11

---
+++ Chapter11/Ex11_1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+#pragma warning(disable : 6031)
+#pragma warning(disable : 4996)
+
+int Max(int arr[]) {
+	int i, maxNum=arr[0];
+	for (i = 0; i < 5; i++) {
+		if (maxNum < arr[i])
+			maxNum = arr[i];
+	}
+	return maxNum;
+}
+
+int Min(int arr[]) {
+	int i, minNum = arr[0];
+	for (i = 0; i < 5; i++) {
+		if (minNum > arr[i])
+			minNum = arr[i];
+	}
+	return minNum;
+}
+
+int Sum(int arr[]) {
+	int i, sum = 0;
+	for (i = 0; i < 5; i++) {
+		sum += arr[i];
+	}
+	return sum;
+}
+int main(void) {
+	
+	// ex01
+	int arr[5], i;
+
+	for (i = 0; i < 5; i++) {
+		printf("정수를 입력하세요. \n");
+		scanf("%d", &arr[i]);
+	}
+	printf("입력된 정수 중에서 최대값. \n", Max(arr));
+	printf("입력된 정수 중에서 최소값. \n", Min(arr));
+	printf("입력된 정수의 총 합. \n", Sum(arr));
+	return 0;
+}
