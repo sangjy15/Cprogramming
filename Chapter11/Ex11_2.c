b08fdbfffb8e7755058e938fecbcb84981f4a448
sangjy15 <40661788+sangjy15@users.noreply.github.com>
2022-04-23 20:48:35 +0900

    11_2

--- Chapter11/Ex11_2.c
+++ Chapter11/Ex11_2.c
@@ -15,7 +15,7 @@ int main(void) {
 		cnt++;
 	}
 	printf("입력받은 문자열 길이 : %d", cnt);
-	*/
+	
 	// ex02
 	char str[20];
 	int length = 0;
@@ -28,5 +28,22 @@ int main(void) {
 	for (i = length - 1; i >= 0; i--) {
 		printf("%c ",str[i]);
 	}
+	*/
+	// ex03
+	char voca[100];
+	int len = 0, i;
+	char max = 0;
+
+	printf("영단어 입력 : ");
+	scanf("%s", voca);
+
+	while (voca[len] != '\0')
+		len++;
+
+	for (i = 0; i < len; i++)
+		if (max < voca[i])
+			max = voca[i];
+
+	printf("가장 큰 아스키 코드 값 문자 : %c \n", max);
 	return 0;
 }
